ff4bbf21b0ace89d78ee394d3dcab5fcc999a5b5
Luiza Breier <luizabreier21@gmail.com>
2015-10-05 16:06:46 -0300

    Corrige o link do botão de Continuar anônimo

--- src/components/UserAuthentication/index.js
+++ src/components/UserAuthentication/index.js
@@ -59,7 +59,7 @@ export default class Authentication extends React.Component {
             </div>
 
             <div className="anonymous">
-              <button className="btn btn-default btn-lg btn-block btn-bottom" to="/shop" onClick={this.handleAnonymous}>Continuar anônimo</button>
+              <Link className="btn btn-default btn-lg btn-block btn-bottom" to="/shop" onClick={this.handleAnonymous}>Continuar anônimo</Link>
             </div>
           </form>
 
