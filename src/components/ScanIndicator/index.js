529f7a01b07ada72e423e7f8d71769c1a797fa07
Augusto Barbosa <augustocbarbosa@gmail.com>
2015-10-06 20:16:36 -0300

    Adiciona botão animado

--- src/components/ScanIndicator/index.js
+++ src/components/ScanIndicator/index.js
@@ -10,6 +10,7 @@ export default class ScanIndicator extends React.Component {
         <div className="text">Adicione os produtos utilizando o leitor</div>
         <div className="image-wrapper">
           <img className="image" src={pinpad}/>
+          <div className="button-ref"></div> 
         </div>
       </div>
     );
