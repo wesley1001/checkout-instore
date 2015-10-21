6267a4f2759f9108ab3a2f44c87531f0d3508cf9
Augusto Barbosa <augustocbarbosa@gmail.com>
2015-10-21 18:48:08 -0200

    Altera tela do barcode

--- src/components/ScanIndicator/index.js
+++ src/components/ScanIndicator/index.js
@@ -7,31 +7,14 @@ export default class ScanIndicator extends React.Component {
   render() {
     return (
       <div className="ScanIndicator component">
-        <div className="image-wrapper">
-          <div className="barcode-laser hide">
-            <span className="barcode-laser-light"></span>
-            <span className="barcode-laser-light"></span>
-            <span className="barcode-laser-light"></span>
-            <span className="barcode-laser-light"></span>
-            <span className="barcode-laser-light"></span>
-          </div>
+        <div className="barcode-laser-bar"></div>
 
-          <div className="barcode-laser-bar"></div>
+        <div className="barcode-button barcode-button-left"></div>
+        <div className="barcode-button barcode-button-right"></div>
 
-          <div className="barcode-line">
-            <div className="on"></div>
-            <div className="off"></div>
-            <div className="starter-bg"></div>
-            <div className="starter"></div>
-          </div>
-
-          <div className="barcode-line-up">
-            <div className="on"></div>
-            <div className="off"></div>
-          </div>
-
-          <div className="barcode-button barcode-button-left"></div>
-          <div className="barcode-button barcode-button-right"></div>
+        <div className="barcode-message">
+          <h2 className="title">Pressione os botões laterais</h2>
+          <div className="subtitle">para adicionar produtos</div>
         </div>
       </div>
     );
