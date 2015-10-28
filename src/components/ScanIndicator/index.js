6f9fb1a13fcfe3a3628d73b01fd3c13e1e28430d
Luiza Breier <luizabreier21@gmail.com>
2015-10-28 14:49:38 -0200

    Retira componente TypeBarcodeReader do ScanIndicator

--- src/components/ScanIndicator/index.js
+++ src/components/ScanIndicator/index.js
@@ -1,58 +1,18 @@
 import React from 'react';
+import TypeBarcodeReader from 'components/TypeBarcodeReader';
 
 import './index.less';
 import pinpad from 'assets/images/Pinpad_Bip.svg';
 
 export default class ScanIndicator extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {ean: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.checkEmail = this.checkEmail.bind(this);
-  }
-
-  showsBarcodeType() {
-    document.getElementById('loga').className='input-group';
-    document.getElementById('texto').className='hidden';
-  }
-
-  handleChange(e) {
-    console.log(e.target.value);
-    this.setState({ean: e.target.value});
-  }
-
-  checkEmail(e) {
-    e.preventDefault();
-    window.handleBarcodeRead(this.state.ean);
-  }
-
   render() {
     return (
       <div className="ScanIndicator component">
-      <div className="hidden" id="loga">
-        <input
-          className="form-control"
-          placeholder="Código de barras"
-          value={this.state.ean}
-          handleChange={this.handleChange}
-          autoComplete="off"
-        />
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-primary" onClick={this.checkEmail}>OK</button>
-        </span>
-      </div>
-        <div className="text" id="texto">Adicione os produtos utilizando o leitor</div>
+        <TypeBarcodeReader/>
+        <div className="text hidden" id="ScanIndicatorForm"><br/>ou<br/><br/></div>
+        <div className="text">Adicione os produtos<br/>utilizando o leitor</div>
         <div className="image-wrapper">
           <img className="image" src={pinpad}/>
-        </div>
-      <div className="productScan">
-<<<<<<< HEAD
-        <button className="btn btn-default btn-lg btn-block btn-bottom" onClick={this.showsBarcodeType}>Digitar código do produto</button>
-=======
-        <button className="btn btn-default btn-lg btn-block btn-bottom" onClick={this.showsBarcodeType}>Digite o código do produto</button>
->>>>>>> ac94bbba7340f107f6a187c2bc598a91dcc42beb
         </div>
       </div>
     );
