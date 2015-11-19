9c7ce6713cfa394c588af5806ee6ee8ae208227f
Tiago Neves <tiago.neves@vtex.com.br>
2015-11-18 22:39:01 -0200

    fix webpack dist with correct appcache name and config

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -31,7 +31,13 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new AppCachePlugin({
-      output: 'instore.appcache'
+      cache: [
+        "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css",
+        "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css",
+        "https://io.vtex.com.br/front-libs/jquery/2.1.3/jquery.min.js",
+        "https://vtexid.vtex.com.br/api/vtexid/pub/authentication/vtexid.min.js"
+      ],
+      output: 'appcache.mf'
     })
   ],
 
