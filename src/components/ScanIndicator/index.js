552b3b7a1a08bb3df09de3e0f79f47c5171c62b2
Augusto Barbosa <augustocbarbosa@gmail.com>
2015-11-25 13:21:43 -0200

    Exibe input de EAN quando não há produtos no cart

--- src/components/ScanIndicator/index.js
+++ src/components/ScanIndicator/index.js
@@ -2,13 +2,47 @@ import React from 'react';
 
 import './index.less';
 import pinpad from 'assets/images/Pinpad_Bip.svg';
+import CheckoutStore from 'stores/CheckoutStore';
 import TypeBarcodeReaderShowButton from 'components/TypeBarcodeReaderShowButton';
+import TypeBarcodeReader from 'components/TypeBarcodeReader';
 
 export default class ScanIndicator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkout: CheckoutStore.getState()
+    };
+
+    this.onCheckoutChange = this.onCheckoutChange.bind(this);
+  }
+
+  onCheckoutChange(state) {
+    this.setState({checkout: state});
+  }
+
+  componentDidMount() {
+    CheckoutStore.listen(this.onCheckoutChange);
+  }
+
+  componentWillUnmount() {
+    CheckoutStore.unlisten(this.onCheckoutChange);
+  }
+
   render() {
+    let showTypeBarReaderForm = this.state.checkout.get('showTypeBarReaderForm');
+    let alternativeText;
+
+    if (showTypeBarReaderForm) {
+      alternativeText = (<div className="text" id="ScanIndicatorForm"><br/>ou<br/><br/></div>);
+    } else {
+      alternativeText = null;
+    }
+
     return (
       <div className="ScanIndicator component">
-        <div className="text hidden" id="ScanIndicatorForm"><br/>ou<br/><br/></div>
+        <TypeBarcodeReader/>
+        {alternativeText}
         <div className="text">Adicione os produtos<br/>utilizando o leitor</div>
         <div className="image-wrapper">
           <img className="image" src={pinpad}/>
