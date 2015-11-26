0c59505c957b18b6c49e10bb545beb7072ede13d
resendefreitas <guilherme.freitas@vtex.com.br>
2015-11-26 21:18:11 -0200

    Adiciona email do cliente no OP

--- src/pages/OrderPlaced.js
+++ src/pages/OrderPlaced.js
@@ -52,7 +52,10 @@ export default class HomePage extends React.Component {
           <hr/>
 
           <div className="receipts">
-            <p>Comprovantes enviados para <strong>nome.sobrenome@gmail.com</strong></p>
+            <p>Comprovantes enviados para <br/><strong>
+            {this.state.checkout.get('customerEmail') === '' ?
+            'Email do cliente não inserido' : this.state.checkout.get('customerEmail')}
+            </strong></p>
             <p>
               <button className="btn btn-default">Imprimir comprovantes</button>
             </p>
