424da0d48202215ba8655beb051f5e529381f512
resendefreitas <guilherme.freitas@vtex.com.br>
2015-11-26 15:39:38 -0200

    Retira dependencia da store dos componentes

--- src/components/TypeBarcodeReader/index.js
+++ src/components/TypeBarcodeReader/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import CheckoutActions from 'actions/CheckoutActions';
-import CheckoutStore from 'stores/CheckoutStore';
 
 export default class TypeBarcodeReader extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      ean: '',
-      checkout: CheckoutStore.getState()
+      ean: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.onCheckoutChange = this.onCheckoutChange.bind(this);
   }
 
   handleChange(e) {
@@ -27,37 +24,13 @@ export default class TypeBarcodeReader extends React.Component {
 
     CheckoutActions.findProduct(this.state.ean);
     this.setState({
-      ean: '',
-      checkout: CheckoutStore.getState()
+      ean: ''
     });
   }
 
-  onCheckoutChange(state) {
-    this.setState({checkout: state});
-  }
-
-  componentDidMount() {
-    CheckoutStore.listen(this.onCheckoutChange);
-  }
-
-  componentWillUnmount() {
-    CheckoutStore.unlisten(this.onCheckoutChange);
-  }
-
-  componentDidUpdate() {
-    let showTypeBarReaderForm = this.state.checkout.get('showTypeBarReaderForm');
-    if (showTypeBarReaderForm && this.refs.barcodeInputType) {
-      this.refs.barcodeInputType.focus();
-    }
-  }
-
   render() {
-    let showTypeBarReaderForm = this.state.checkout.get('showTypeBarReaderForm');
-
-    let form;
-
-    if (showTypeBarReaderForm) {
-      form = (
+    return (
+      <div className="TypeBarcodeReader component container">
         <form className="text-left" id="TypeBarcodeReaderForm" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <div className="input-group">
@@ -76,15 +49,6 @@ export default class TypeBarcodeReader extends React.Component {
             </div>
           </div>
         </form>
-      );
-    } else {
-      form = null;
-    }
-
-    return (
-      <div className="TypeBarcodeReader component container">
-        {showTypeBarReaderForm}
-        {form}
       </div>
     );
   }
--- src/components/TypeBarcodeReaderShowButton/index.js
+++ src/components/TypeBarcodeReaderShowButton/index.js
@@ -4,35 +4,16 @@ import { Link } from 'react-router';
 import CheckoutActions from 'actions/CheckoutActions';
 
 export default class TypeBarcodeReaderShowButton extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      visible: true
-    };
-
-    this.showsBarcodeType = this.showsBarcodeType.bind(this);
-  }
 
   showsBarcodeType() {
-    this.setState({ visible: false });
-    CheckoutActions.showTypeBarReaderForm(true);
+    console.log('COMPONENT CLICK');
+    CheckoutActions.showTypeBarReaderForm();
   }
 
   render() {
-    let button;
-
-    if (this.state.visible === true) {
-      button = (
-        <button className="btn btn-default btn-lg btn-block btn-bottom" onClick={this.showsBarcodeType}>Digitar código do produto</button>
-      );
-    } else {
-      button = null;
-    }
-
     return (
       <div id="TypeBarcodeReaderShowButton" className="TypeBarcodeReaderShowButton component">
-        {button}
+        <button className="btn btn-default btn-lg btn-block btn-bottom" onClick={this.showsBarcodeType}>Digitar código do produto</button>
       </div>
     );
   }
