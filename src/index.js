de1b499cbb722980c2a0c068b843815d637b9003
Tiago Neves <tiago.neves@vtex.com.br>
2015-12-28 15:34:08 -0200

    remove old code comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router } from 'react-router';
-// import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
-
-// let history = createBrowserHistory();
 
 const container = document.getElementById('app-container');
 
 render(<Router>{routes}</Router>, container);
-// React.render(<Router history={history}>{routes}</Router>, container);
