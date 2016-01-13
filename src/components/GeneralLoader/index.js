09b25634a6481c0a54b695fdf33e8b5997988976
Tiago Neves <tiago.neves@vtex.com.br>
2016-01-13 18:50:39 -0200

    fix loader show/hide

--- src/components/GeneralLoader/index.js
+++ src/components/GeneralLoader/index.js
@@ -4,17 +4,58 @@ import './index.less';
 import loader from 'assets/images/loader.svg';
 
 export default class Loader extends React.Component {
-  render() {
-    const spinner = (
-      <div className="wrapper">
-        <img className="spinner" src={loader} width="120"/>
-      </div>
-    );
 
-    return (
-      <div className="GeneralLoader component">
-        {this.props.loading ? spinner : ''}
-      </div>
-    );
+  showLoader(){
+    if(window.WebViewBridge){
+      window.WebViewBridge.send(JSON.stringify({
+        type: 'event',
+        event: 'showLoader'
+      }));
+    }
+  }
+
+  hideLoader(){
+    if(window.WebViewBridge){
+      window.WebViewBridge.send(JSON.stringify({
+        type: 'event',
+        event: 'hideLoader'
+      }));
+    }
+  }
+
+  componentDidMount(){
+    if(this.props.loading){
+      this.showLoader();
+    }
+  }
+
+  componentDidUpdate(prevProps){
+    if(prevProps.loading != this.props.loading){
+      if(this.props.loading === true){
+        this.showLoader();
+      }
+      else{
+        this.hideLoader();
+      }
+    }
+  }
+
+  render() {
+    if(window.WebViewBridge){
+      return (<div></div>);
+    }
+    else{
+      const spinner = (
+        <div className="wrapper">
+          <img className="spinner" src={loader} width="120"/>
+        </div>
+      );
+
+      return (
+        <div className="GeneralLoader component">
+          {this.props.loading ? spinner : ''}
+        </div>
+      );
+    }
   }
 }
