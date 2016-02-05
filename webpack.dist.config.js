65cb62f00c5eb09c38789a08f6178e5817ff2caa
Tiago Neves <tiago.neves@vtex.com.br>
2016-02-05 14:12:11 -0200

    enhance build compression

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -27,7 +27,12 @@ module.exports = {
 
   plugins: [
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}}),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new AppCachePlugin({
