46e9f6dc2ca284735d3d62560fc99a3257297441
Luiza Breier <luizabreier21@gmail.com>
2016-02-23 17:13:37 -0300

    Create new folder component ProductAdder

---
+++ src/components/ProductAdder/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.less';
+
+export default class ProductAdder extends React.Component {
+  render() {
+    return (
+      <div className="NewProducts component">
+        <a className="text">Adicionar novos produtos</a>
+      </div>
+    );
+  }
+}
