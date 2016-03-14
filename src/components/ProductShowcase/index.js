57414995a3302fe79bce45f633f8eba2b9c2d7ee
resendefreitas <guilherme.freitas@vtex.com.br>
2016-03-14 14:44:42 -0300

    Barcode typing in cart page

--- src/components/ProductShowcase/index.js
+++ src/components/ProductShowcase/index.js
@@ -1,8 +1,14 @@
 import React, {PropTypes} from 'react';
 
+import CheckoutStore from 'stores/CheckoutStore';
+import CheckoutActions from 'actions/CheckoutActions';
+
 import ProductList from 'components/ProductList';
 import OrderHeader from 'components/OrderHeader';
+import TypeBarcodeReaderShowButton from 'components/TypeBarcodeReaderShowButton';
+import TypeBarcodeReader from 'components/TypeBarcodeReader';
 
+import './index.less';
 
 export default class ProductShowcase extends React.Component {
   static propTypes = {
@@ -13,12 +19,44 @@ export default class ProductShowcase extends React.Component {
     products: []
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkout: CheckoutStore.getState()
+    };
+
+    this.onCheckoutChange = this.onCheckoutChange.bind(this);
+  }
+
+  onCheckoutChange(state) {
+    this.setState({checkout: state});
+  }
+
+  componentDidMount() {
+    CheckoutStore.listen(this.onCheckoutChange);
+  }
+
+  componentWillUnmount() {
+    CheckoutStore.unlisten(this.onCheckoutChange);
+  }
+
+  showsBarcodeType() {
+    CheckoutActions.showTypeBarReaderForm();
+    CheckoutActions.hideTypeEmailForm();
+  }
+
   render() {
     const {products, orderFormId, history} = this.props;
 
     return (
       <section className="ProductShowcase component">
         <OrderHeader />
+        {
+          this.state.checkout.get('typingBarcode') ? '' :
+          <button className="btn btn-default btn-lg type-button" onClick={this.showsBarcodeType}>Digitar código do produto</button>
+        }
+        {this.state.checkout.get('typingBarcode') ? <TypeBarcodeReader /> : ''}
 
         <section>
           <ProductList
